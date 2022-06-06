5c3e070eeb54445c6c8b860d3c2b7153a8c9ab0f
Im-Hass <huiseon4107@gmail.com>
2022-06-07 03:25:40 +0900

    feat: HomeView에서는 HeaderNav 숨기기

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="app">
-    <header-nav></header-nav>
+    <header-nav v-if="!homeCome"></header-nav>
     <router-view id="main" />
   </div>
 </template>
 <script>
 import HeaderNav from "@/components/HeaderNav.vue";
+
 export default {
   name: "App",
   components: {
     HeaderNav,
+  },
+  data() {
+    return {
+      isHome: true,
+    };
+  },
+  computed: {
+    homeCome() {
+      return this.isHome;
+    },
   },
 };
 </script>
